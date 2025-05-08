c10fe3a949786cc303bcfbf5d69b36fc3c9ec967
sraawan-sai <sraawan.sai@gmail.com>
2025-05-08 10:26:55 +0000

    add uuid module

--- controllers/kycController.js
+++ controllers/kycController.js
@@ -1,7 +1,7 @@
 // src/controllers/kycController.js
 
 import { KycRequestModel, UserModel } from '../models/Schema.js';
-import { v4 as uuidv4 } from 'uuid';
+const { v4: uuidv4 } = require('uuid');
 
 
 // Get all KYC requests
